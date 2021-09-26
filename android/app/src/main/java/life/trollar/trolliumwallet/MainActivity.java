c601833dda76fe7ffbef02ae1e86c4e72be89e8a
tired6488 <85363080+tired6488@users.noreply.github.com>
2021-09-25 22:10:37 -0500

    Revert "recommitting natrium"
    This reverts commit f89a0c511f93c21866cde26eb0f8203581a65ec6.

---
+++ android/app/src/main/java/life/trollar/trolliumwallet/MainActivity.java
@@ -0,0 +1,30 @@
+package life.trollar.trolliumwallet;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterMain;
+
+public class MainActivity extends FlutterFragmentActivity {
+    private static final String CHANNEL = "fappchannel";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+            .setMethodCallHandler(
+                (call, result) -> {
+                    if (call.method.equals("getSecret")) {
+                        //result.success(new LegacyStorage().getSecret());
+                        result.notImplemented();
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+            );
+    }
+}
--- ios/Classes/FlutterNanoFfiPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterNanoFfiPlugin : NSObject<FlutterPlugin>
-@end
